f36868c1ccd8b5db297b1756d7770d15f464b4bd
Gerald Gicharu <gicharugerald@gmail.com>
2023-09-19 20:27:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+
+//returns the number of blocks given a value
+function distanceFromHqInBlocks(someValue) {
+    const hqLocation = 42;
+    return Math.abs(someValue - hqLocation)
+  }
+
+  function distanceFromHqInFeet(someValue) {
+    const blocks = distanceFromHqInBlocks(someValue);
+    const feetPerBlock = 264;  
+    return blocks * feetPerBlock
+  }
+
+  //returns the number of feet traveled
+  function distanceTravelledInFeet(start, destination) {
+    const feetPerBlock = 264;
+    return Math.abs(destination - start) * feetPerBlock
+  }
+
+  //fare for the customer
+  function calculatesFarePrice(start, destination) {
+    const feetPerBlock = 264;
+    const distanceInFeet = Math.abs(destination - start) * feetPerBlock;
+
+    if (distanceInFeet <= 400) {
+        return 0;
+    } else if (distanceInFeet <= 2000) {
+        return (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
